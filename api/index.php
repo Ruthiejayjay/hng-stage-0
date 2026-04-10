2458e8fe1da92c66f2cdb6dd576d8436e34b1b7f
Ruthiejayjay <jamesruth449@gmail.com>
2026-04-10 16:59:39 +0100

    added env to test deployment on laravel cloud

--- api/index.php
+++ api/index.php
@@ -1,19 +1,15 @@
 <?php
-error_reporting(E_ALL);
-ini_set('display_errors', '1');
 
-echo "PHP is working\n";
+define('LARAVEL_START', microtime(true));
 
-// Test 1: Can we find autoload?
-if (!file_exists(__DIR__ . '/../vendor/autoload.php')) {
-    die("ERROR: vendor/autoload.php not found. Path: " . __DIR__);
-}
-echo "Autoload found\n";
-
-// Test 2: Can we load it?
 require __DIR__ . '/../vendor/autoload.php';
-echo "Autoload loaded\n";
 
-// Test 3: Can we boot Laravel?
 $app = require_once __DIR__ . '/../bootstrap/app.php';
-echo "Laravel booted\n";
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$request = Illuminate\Http\Request::capture();
+$response = $kernel->handle($request);
+$response->send();
+
+$kernel->terminate($request, $response);
